63fbcd35a01aae47d047661dbe2aa2f7eddbe286
wooddie <n1ght.d1ck1ns@gmail.com>
2026-03-31 15:54:20 +0500

    update table & code

--- http_server.cpp
+++ http_server.cpp
@@ -4,7 +4,6 @@
 #define CPPHTTPLIB_NO_EXCEPTIONS
 #include "lib/httplib.h"
 #include "sqlite3.h"
-#include <ctime>
 #include <iomanip>
 #include <sstream>
 
@@ -17,32 +16,34 @@ int main() {
     }
 
     const char *createTableSQL =
-            "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, coordinates TEXT, client_ip TEXT, created_at TEXT);";
+            "CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY, user_name TEXT, coordinates TEXT, client_ip TEXT, created_at TEXT);";
     sqlite3_exec(db, createTableSQL, nullptr, nullptr, nullptr);
 
     httplib::Server server;
 
     server.Get("/hi", [](const httplib::Request &req, httplib::Response &res) {
-        res.set_content("<h1>Hello user!</h1><form ...>...</form>", "text/html");
+        res.set_content(R"(
+        <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; min-height: 100vh; text-align: center; gap: 20px;">
+            <h1>Hello user!</h1>
 
-        res.set_content(
-            "<form action='/PostData' method='POST'>"
-            "  <input name='coordinates' type='text'>"
-            "  <button type='submit'>Send coordinates</button>"
-            "</form>",
-            "text/html"
-        );
+            <form action="/PostData" method="POST">
+                <input name="user_name" type="text" placeholder="Name">
+                <input name="coordinates" type="text" placeholder="Coordinates">
+                <button type="submit">Send coordinates</button>
+            </form>
+        </div>
+    )", "text/html");
     });
 
 
-
     server.Post("/PostData", [db](const httplib::Request &req, httplib::Response &res) {
+        std::string user_name = req.get_param_value("user_name");
         std::string coordinates = req.get_param_value("coordinates");
         std::string client_ip = req.remote_addr;
 
-        std::cout << "Получен POST от " << client_ip << ": " << coordinates.c_str() << std::endl;
+        std::cout << "Получен POST от " << client_ip << ": " << coordinates.c_str() << " : " << user_name.c_str() << std::endl;
 
-        const char *sql = "INSERT INTO messages (coordinates, client_ip, created_at) VALUES (?, ?, ?);";
+        const char *sql = "INSERT INTO messages (user_name, coordinates, client_ip, created_at) VALUES (?, ?, ?, ?);";
         sqlite3_stmt *stmt;
 
         auto t = std::time(nullptr);
@@ -53,9 +54,10 @@ int main() {
         std::string current_time = oss.str();
 
         if (sqlite3_prepare_v2(db, sql, -1, &stmt, nullptr) == SQLITE_OK) {
-            sqlite3_bind_text(stmt, 1, coordinates.c_str(), -1, SQLITE_TRANSIENT);
-            sqlite3_bind_text(stmt, 2, client_ip.c_str(), -1, SQLITE_TRANSIENT); // IP сюда
-            sqlite3_bind_text(stmt, 3, current_time.c_str(), -1, SQLITE_TRANSIENT); // Время сюда
+            sqlite3_bind_text(stmt, 1, user_name.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 2, coordinates.c_str(), -1, SQLITE_TRANSIENT);
+            sqlite3_bind_text(stmt, 3, client_ip.c_str(), -1, SQLITE_TRANSIENT); // IP сюда
+            sqlite3_bind_text(stmt, 4, current_time.c_str(), -1, SQLITE_TRANSIENT); // Время сюда
 
             if (sqlite3_step(stmt) == SQLITE_DONE) {
                 res.status = 201; // Created
